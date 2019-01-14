bd181aa8cc865a87502354d2e5a86ec7b4b09e43
Michael Mansperger <ManspergerMichael@gmail.com>
2019-01-14 15:36:00 -0800

    data types

--- cTutorial/Program.cs
+++ cTutorial/Program.cs
@@ -6,12 +6,31 @@ namespace cTutorial
     {
         static void Main(string[] args)
         {
-           //System.Console.WriteLine("Hello World");
+            //System.Console.WriteLine("Hello World");
 
-           //console input
-           Console.Write("What is your name?");
-           string name = Console.ReadLine();
-           System.Console.WriteLine("Hello "+name);
+            //console input
+            /* Console.Write("What is your name?");
+            string name = Console.ReadLine();
+            System.Console.WriteLine("Hello "+name); */
+
+            //data Types
+
+            bool canVote = true;
+            char grade = 'A';
+
+            //interger with max number of 2,147,483,647
+            int maxInt = int.MaxValue;
+            //large numbers
+            long maxLong;
+            decimal Dec;
+            
+            //32 bit, 7 levels of presicion
+            float floatInt;
+            //32 bit number with 15 decimals of presicion
+            double Dubs;
+
+
+
         }
     }
 }
