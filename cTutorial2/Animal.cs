b42b546c7bc92b9415680162a730c8d5ae1167f6
Michael Mansperger <ManspergerMichael@gmail.com>
2019-01-16 14:16:58 -0800

    classes

--- cTutorial2/Animal.cs
+++ cTutorial2/Animal.cs
@@ -6,5 +6,13 @@ namespace cTutorial2
         public double weight {get; set;}
         public string sound {get; set;}
         public double name {get; set;}
+
+        public Animal()
+        {
+            this.height = 0;
+            this.weight = 0;
+            this.sound = "No sound";
+            this.sound = "No name";
+        }
     }
 }
