95798323bd1809d3341aa787e675616e5454db94
Michael Mansperger <ManspergerMichael@gmail.com>
2019-01-16 14:18:44 -0800

    constructors

--- cTutorial2/Animal.cs
+++ cTutorial2/Animal.cs
@@ -7,12 +7,21 @@ namespace cTutorial2
         public string sound {get; set;}
         public double name {get; set;}
 
+        //no argument constuctor
         public Animal()
         {
             this.height = 0;
             this.weight = 0;
             this.sound = "No sound";
             this.sound = "No name";
+        }
+        //constructor with args
+        public Animal(double height, double weight, string name, string sound)
+        {
+            this.height = height;
+            this.weight = weight;
+            this.sound = sound;
+            this.sound = name;
         }
     }
 }
