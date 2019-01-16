19229950c48d47277451e57e613e020507c4dac6
Michael Mansperger <ManspergerMichael@gmail.com>
2019-01-16 14:15:05 -0800

    classes review

---
+++ cTutorial2/Animal.cs
@@ -0,0 +1,10 @@
+namespace cTutorial2
+{
+    class Animal
+    {
+        public double height {get; set;}
+        public double weight {get; set;}
+        public string sound {get; set;}
+        public double name {get; set;}
+    }
+}
